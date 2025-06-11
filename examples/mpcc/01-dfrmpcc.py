17074bd2165570012da80deca8c600f0d50e1bb7
Fabian Faulstich <fabianfaulstich@drumlin-197.dynamic.rpi.edu>
2025-06-11 13:31:16 -0400

    improved ll structure and localization

--- examples/mpcc/01-dfrmpcc.py
+++ examples/mpcc/01-dfrmpcc.py
@@ -1,6 +1,11 @@
 from pyscf import gto, scf, cc
 from pyscf.mp.dfmp2_native import DFMP2
 
+from pyscf.mcscf import avas
+from pyscf.data.elements import chemcore
+
+import numpy as np
+
 from pyscf import mpcc
 
 if __name__ == "__main__":
@@ -11,10 +16,47 @@ if __name__ == "__main__":
         [1, (0.0, -0.757, 0.587)],
         [1, (0.0, 0.757, 0.587)],
     ]
-    mol.basis = "cc-pvdz"
+    mol.basis = "cc-pvtz"
     mol.build()
 
     mf = scf.RHF(mol).density_fit().run()
+
+    # Orbital localization    
+
+
+    
+    ncore = chemcore(mol)
+    nelec_as = tuple(nelec - ncore for nelec in mol.nelec)
+    n_cas = mol.nao - ncore
+    active_orbs = [p for p in range(ncore, mol.nao)]
+    frozen_orbs = [i for i in range(mol.nao) if i not in active_orbs]
+
+    breakpoint()
+
+    ao_labels = ["O 2p", "O 2s","O 3p", "O 3s"]
+    minao="sto-3g"
+    openshell_option = 3
+    
+    avas_obj = avas.AVAS(mf, ao_labels, minao=minao, openshell_option=openshell_option)
+    avas_obj.with_iao = True
+    avas_obj.threshold = 1e-7 
+    
+    _, _, mocas = avas_obj.kernel()
+    act_hole = (
+        np.where(avas_obj.occ_weights > avas_obj.threshold)[0]
+    )
+
+    act_part = (
+        np.where(avas_obj.vir_weights > avas_obj.threshold)[0] + mf.mol.nelec[0])
+
+    c_lo = mocas
+
+    print ("dimension of active hole", len(act_hole)) 
+    print ("dimension of active part", len(act_part)) 
+
+    frag = [[act_hole, act_part]]
+
+
     mymp = DFMP2(mf).run()
     #mycc = cc.CCSD(mf).density_fit().run()
 
@@ -29,9 +71,11 @@ if __name__ == "__main__":
     mympcc.lowlevel.ll_con_tol = 1e-6
 
 
-    mympcc.kernel()
+    # localization input
+    # 
+    mympcc.kernel(localization = True, )
 
-    print("Left MPCC")
+    print("Quit MPCC")
     print(mympcc.lowlevel.e_tot)
     breakpoint()
     # localization, where?
