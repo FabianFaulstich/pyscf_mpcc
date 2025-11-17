483753eeecc4372bb9b02ee8c470352a16260102
FabianFaulstich <fabianfaulstich@MacBookPro.lan>
2025-11-16 20:51:01 -0500

    added AO labeling for avas

--- examples/mpcc/03-low_level_only.py
+++ examples/mpcc/03-low_level_only.py
@@ -1,6 +1,11 @@
 from pyscf import gto, scf, cc
 from pyscf import mpcc
 
+from pyscf.mpcc import mpcc_tools as mpt
+
+from pyscf.mcscf import avas
+
+import numpy as np
 import time
 
 if __name__ == "__main__":
@@ -13,10 +18,26 @@ if __name__ == "__main__":
     ]
     mol.basis = "cc-pvdz"
     mol.build()
-
     mf = scf.RHF(mol).density_fit().run()
-    c_lo = mf.mo_coeff
- 
+
+    # Generating LO basis 
+    ao_labels = mpt.get_ao_labels(mol)
+    minao="sto-3g"
+    openshell_option = 3
+
+    ncore = 0
+    nelec_as = tuple(nelec - ncore for nelec in mol.nelec)
+    n_cas = mol.nao - ncore
+    active_orbs = [p for p in range(ncore, mol.nao)]
+    frozen_orbs = [i for i in range(mol.nao) if i not in active_orbs]
+
+    avas_obj = avas.AVAS(mf, ao_labels, minao=minao, openshell_option=openshell_option)
+    avas_obj.with_iao = True
+    avas_obj.threshold = 1e-7 
+    
+    _, _, c_lo = avas_obj.kernel()
+
+    # Generating empty fragment infor
     frag_info = {'frag': [[[0], [0]]]}
     conv_info = {'ll_con_tol': 1e-6, 'll_max_its': 80}
     kwargs = frag_info|conv_info  #union operation
